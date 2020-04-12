af369d2408658395d10aede8fa06d4907f6eca98
Deepak Lamba <lamba.deepak15@gmail.com>
2020-04-12 17:14:32 +0530

    producer

---
+++ kafka-twitter-producer/src/main/java/com/deepak/kafka/twitter/TwitterCredentials.java
@@ -0,0 +1,13 @@
+package com.deepak.kafka.twitter;
+
+public class TwitterCredentials {
+	
+	public static final String CONSUMER_KEY = "";
+	
+	public static final String CONSUMER_SECRET = "";
+	
+	public static final String TOKEN = "";
+	
+	public static final String SECRET = "";
+
+}
---
+++ kafka-twitter-producer/src/test/java/com/deepak/kafka/twitter/TestTwitterProducer.java
@@ -0,0 +1,6 @@
+package com.deepak.kafka.twitter;
+
+
+public class TestTwitterProducer {
+
+}
